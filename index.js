baa611f4ba1e1f8a79425d48faec16e4d5b61342
Saqlain Gardezi <saqlaingardezi86@gmail.com>
2017-03-09 10:31:45 +0500

    disconnecting users

--- index.js
+++ index.js
@@ -41,28 +41,51 @@ app.use('/index', index);
 // });
 
 var users=[];
+var thisUser=[];
 io.on('connection', function(socket){
   console.log('a user connected');
 
-    
   socket.on('chat message', function(msg, person){
-  	//	send to everyone except someone usee...
-  	//	socket.broadcast.emit('hi');
+    //  send to everyone except someone usee...
+    //  socket.broadcast.emit('hi');
 
       io.emit('chat message', msg, person );
    
     //  console.log('message : ', msg);
   });
   socket.on('disconnect', function(){
-  	console.log('A User disconnected');
+    console.log('A User disconnected with name :: ' + thisUser);
+    console.log("delete user with id== "+ thisUser[socket.id]);
+    for(var i=0; i<users.length; i++){
+      if(users[i] == thisUser[socket.id]){
+        console.log("before splice :: " + users);
+        users.splice(i,1);
+        console.log("after splice :: " + users);
+        io.sockets.emit('userDisconnected', users);
+      }
+      else{
+        console.log("deleting session");
+      }
+    }
+    // for(var i=0; i< users.length; i++){
+    //   if (thisUser == users[i]) {
+    //     users.splice(i,1);
+    //     console.log("after splice :: " + users);
+    //     io.sockets.emit('userDisconnected', users);
+    //   }
+    // }
   });
 
+ 
+
   socket.on('new user', function(user){
-      
+
+        thisUser[socket.id]=user;
         users.push(user);
     //updateClients(users);
       console.log(users);
 //      io.sockets.send('userAdded', users);
+console.log("length is :: " +users.length);
       io.sockets.emit('userAdded', users);
 
     
